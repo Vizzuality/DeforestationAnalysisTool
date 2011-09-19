e44fe1fda2ccb1705bfc476180a2f89978cc882a
javi <qualopec@gmail.com>
2011-09-19 15:51:57 +0200

    missing assets

---
+++ src/static/js/vis/popup.js
@@ -0,0 +1,15 @@
+
+//
+// map popup that shows degradation and deforestation stats
+var MapPopup = Backbone.View.extend({
+
+    el: $(".map_container #popup"),
+
+    initialize: function() {
+    },
+
+    // shows the popup with info
+    showAt: function(latLng, info) {
+    }
+
+});
