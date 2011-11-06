038632da6d9d5619cdace60f4b1552971e8f0fdd
javi santana <qualopec@gmail.com>
2011-11-06 21:29:33 +0100

    added report for 2011_09

--- src/application/generate_reports.py
+++ src/application/generate_reports.py
@@ -29,12 +29,12 @@ assets_id = ['SAD_VALIDATED/SAD_2009_08',
 'SAD_VALIDATED/SAD_2011_05',
 'SAD_VALIDATED/SAD_2011_06',
 'SAD_VALIDATED/SAD_2011_07',
-'SAD_VALIDATED/SAD_2011_08']
-
+'SAD_VALIDATED/SAD_2011_08',
+'SAD_VALIDATED/SAD_2011_09']
 
 
 print "assetid,report_id"
-for r in assets_id:
+for r in assets_id[-1:]:
     first, last = month_range(*reversed(map(int, r.split('_')[-2:])))
     url =  "http://%s/_ah/cmd/create_report?year=%d&month=%d&day=%d&assetid=%s&fyear=%d&fmonth=%d&fday=%d""" % (sys.argv[1], first.year, first.month, first.day, r, last.year, last.month, last.day)
     print url
